20e2ce50e3c37d18682b2ec7d6ce411afe090b7a
chagan <chrishagan23@gmail.com>
2014-08-23 12:26:38 -0500

    Adding functions.php

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php // Opening PHP tag - nothing should be before this, not even whitespace
+
+/**
+ * Co-authors in RSS and other feeds
+ * /wp-includes/feed-rss2.php uses the_author(), so we selectively filter the_author value
+ */
+function db_coauthors_in_rss( $the_author ) {
+
+if ( !is_feed() || !function_exists( 'coauthors' ) )
+   return $the_author;
+
+return coauthors( null, null, null, null, false );
+}
+add_filter( 'the_author', 'db_coauthors_in_rss' );
+
+?>
